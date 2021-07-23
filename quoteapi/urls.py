4a404e033ef58ac1ee340da52ecdec1c141ca8f9
Somiparno Chattopadhyay <28658223+clueless-skywatcher@users.noreply.github.com>
2021-07-23 10:20:23 +0530

    Fix: Authentication issues fixed for running in postman

--- quoteapi/urls.py
+++ quoteapi/urls.py
@@ -24,5 +24,5 @@ router.register(r'quotes', views.QuoteViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
-    path('api-auth', include('rest_framework.urls', namespace = 'rest_framework'))
+    # path('api-auth', include('rest_framework.urls', namespace = 'rest_framework'))
 ]
